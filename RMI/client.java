ebc813b106aa8ac81d0f0ab62047d5e52cbea3e2
Prince Sharma <orewaluffy1469@gmail.com>
2026-08-19 12:16:41 +0530

    Implement RMI client to perform basic arithmetic operations

--- RMI/client.java
+++ RMI/client.java
@@ -1,5 +1,15 @@
-package RMI;
+import java.rmi.Naming;
 
 public class client {
-    
+    public static void main(String[] args) {
+        try {
+            Calculator calc = (Calculator) Naming.lookup("rmi://localhost/Calculator");
+            System.out.println("Addition: " + calc.add(5, 3));
+            System.out.println("Subtraction: " + calc.subtract(5, 3));
+            System.out.println("Multiplication: " + calc.multiply(5, 3));
+            System.out.println("Division: " + calc.divide(5, 3));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
